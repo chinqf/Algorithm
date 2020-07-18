a3c097efab3a5dc9fd670bbb83e671b35f40fe6a
chinqf <3196887366@qq.com>
2020-07-18 15:54:32 +0800

    215. 引入随机化加速

--- leetcode/src/main/java/leetcode/editor/cn/Test.java
+++ leetcode/src/main/java/leetcode/editor/cn/Test.java
@@ -1,12 +1,17 @@
 package leetcode.editor.cn;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
         int[] arr = {5, 6, 2, 1, 9, 7, 3, 11, 0};
         partition(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
+
+        Random rand = new Random();
+        System.out.println(rand.nextInt(3));
+
     }
 
     public static int partition(int[] arr, int left, int right) {
