40a827375565df086a20096d7373fe985c21503f
chinqf <3196887366@qq.com>
2020-08-09 17:16:34 +0800

    110. 平衡二叉树

---
+++ leetcode/src/main/java/leetcode/editor/cn/P110_BalancedBinaryTree_平衡二叉树.java
@@ -0,0 +1,72 @@
+package leetcode.editor.cn;
+
+import common.node.TreeNode;
+
+/**
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * 本题中，一棵高度平衡二叉树定义为：一个二叉树每个节点的左右两个子树的高度差的绝对值不超过1。
+ *
+ * 示例 1:
+ * 给定二叉树 [3,9,20,null,null,15,7]
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回 true 。
+ *
+ * 示例 2:
+ * 给定二叉树 [1,2,2,3,3,null,null,4,4]
+ *        1
+ *       / \
+ *      2   2
+ *     / \
+ *    3   3
+ *   / \
+ *  4   4
+ * 返回 false 。
+ *
+ * Related Topics 树 深度优先搜索
+ */
+public class P110_BalancedBinaryTree_平衡二叉树{
+  public static void main(String[] args) {
+       Solution solution = new P110_BalancedBinaryTree_平衡二叉树().new Solution();
+       
+  }
+  //leetcode submit region begin(Prohibit modification and deletion)
+  class Solution {
+      public boolean isBalanced(TreeNode root) {
+          return recur(root) != -1;
+      }
+
+      public int recur(TreeNode root) {
+          if (root == null) {
+              return 0;
+          }
+          int left = recur(root.left);
+          if (left == -1) return -1;
+          int right = recur(root.right);
+          if (right == -1) return -1;
+          return Math.abs(left - right) > 1 ? -1 : Math.max(left, right) + 1;
+      }
+  }
+  //leetcode submit region end(Prohibit modification and deletion)
+
+  // 执行耗时:1 ms,击败了99.73% 的Java用户
+  public static boolean isBalanced(TreeNode root) {
+    return recur(root) != -1;
+  }
+
+  public static int recur(TreeNode root) {
+    if (root == null) {
+        return 0;
+    }
+    int left = recur(root.left);
+    if (left == -1) return -1;
+    int right = recur(root.right);
+    if (right == -1) return -1;
+    return Math.abs(left - right) > 1 ? -1 : Math.max(left, right) + 1;
+  }
+
+
+}
