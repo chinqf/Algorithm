9494837f64cc24feaf84f5266c7bcea49398fdc7
chinqf <3196887366@qq.com>
2020-08-24 20:58:21 +0800

    217. 存在重复元素

---
+++ leetcode/src/main/java/leetcode/editor/cn/P217_ContainsDuplicate_存在重复元素.java
@@ -0,0 +1,45 @@
+package leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 给定一个整数数组，判断是否存在重复元素。
+ * 如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+ *
+ * 示例 1:
+ * 输入: [1,2,3,1]
+ * 输出: true
+ *
+ * 示例 2:
+ * 输入: [1,2,3,4]
+ * 输出: false
+ *
+ * 示例 3:
+ * 输入: [1,1,1,3,3,4,3,2,4,2]
+ * 输出: true
+ * Related Topics 数组 哈希表
+ */
+public class P217_ContainsDuplicate_存在重复元素 {
+    public static void main(String[] args) {
+        Solution solution = new P217_ContainsDuplicate_存在重复元素().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean containsDuplicate(int[] nums) {
+            // 执行耗时:5 ms,击败了78.33% 的Java用户
+            Set<Integer> set = new HashSet();
+            for (int i = 0; i < nums.length; i++) {
+                if (!set.add(nums[i])) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+}
