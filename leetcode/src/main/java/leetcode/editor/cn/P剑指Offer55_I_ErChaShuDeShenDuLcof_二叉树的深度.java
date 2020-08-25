0e1259114ded448caa9ef6b305fd75b4fbc51b5d
chinqf <3196887366@qq.com>
2020-08-25 20:43:26 +0800

    剑指Offer55_I 二叉树的深度

---
+++ leetcode/src/main/java/leetcode/editor/cn/P剑指Offer55_I_ErChaShuDeShenDuLcof_二叉树的深度.java
@@ -0,0 +1,38 @@
+package leetcode.editor.cn;
+
+import common.node.TreeNode;
+
+/**
+ * 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+ * 例如：给定二叉树 [3,9,20,null,null,15,7]，
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最大深度 3 。
+ *
+ * 提示：节点总数 <= 10000
+ * 注意：本题与主站 104 题相同：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+ * Related Topics 树 深度优先搜索
+ */
+public class P剑指Offer55_I_ErChaShuDeShenDuLcof_二叉树的深度 {
+    public static void main(String[] args) {
+        Solution solution = new P剑指Offer55_I_ErChaShuDeShenDuLcof_二叉树的深度().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return Math.max(left, right) + 1;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
